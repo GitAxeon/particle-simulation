3ea0142e212c75df697b3d939644614bd8adf5c1
Axeon <mipa0010@gmail.com>
2023-08-29 21:04:39 +0300

    Added tmp to .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <SDL3/SDL.h>
 
 int main(int argc, char* argv[])
 {
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cout << "Meh" << std::endl;
+    }
+
     std::cout << "Runnin\n";
     return (EXIT_SUCCESS);
 }
